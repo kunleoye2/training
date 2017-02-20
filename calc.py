1262c27d77d3218c0fce3bb35664c70560b1b2df
kunleoye2 <kunleoye2@yahoo.com>
2017-02-20 13:56:00 -0500

    reversed changes made to script

--- calc.py
+++ calc.py
@@ -4,11 +4,11 @@ def add(x,y):
 def subtract(x,y):
     pass
 
-def multiply(x):
-    return
+def multiply(x,y):
+    return x*y
 
 def divide(x,y):
-    pass
+    return x/y
 
 def square(x):
     pass
